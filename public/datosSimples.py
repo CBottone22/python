46ae0bd8b9c401c021aed87d895caa693b903ebc
BCaroBottone <125994571+BCaroBottone@users.noreply.github.com>
2023-12-14 03:26:02 +0000

    tipos de dfatos en pyhthon

---
+++ public/datosSimples.py
@@ -0,0 +1,17 @@
+"string"
+
+"""Tus datos son:
+   Nombre: Caro; 
+   usando la triple comilla puedo usar saltos de reglón si no marca un error
+"""
+
+"numeros"
+"int"
+44444
+"float"
+4.5
+
+"booleanos"
+True
+False
+

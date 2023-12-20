df326e50d1069fcfd372806e3888b25b4e90d7f0
BCaroBottone <125994571+BCaroBottone@users.noreply.github.com>
2023-12-20 03:26:28 +0000

    opeeradores aritmeticos

---
+++ Variables/Operaedores aritmetico/operadores_aritmeticos.py
@@ -0,0 +1,28 @@
+#suma y resta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+
+#multi y división (* y /)
+multi = 12 * 5
+division = 12 / 6 #devuelve un dato float
+
+#potenciación (exponente) (**)
+exponente = 12 ** 5
+
+#división baja (//)
+division_baja = 12 // 5
+
+#resto o módulo (%)
+resto = 12 % 5
+
+#type(dato) nos devuelve que tipo de dato es
+tipo_de_dato = type(division_baja)
+print (tipo_de_dato)
+
+print (suma)
+print (resta)
+print (multi)
+print (division)
+print (exponente)
+print (division_baja)
+print (resto) 

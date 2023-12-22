211d192802bb6fbfadc1bc98732f6db4ff7858ec
BCaroBottone <125994571+BCaroBottone@users.noreply.github.com>
2023-12-22 04:47:50 +0000

    else if(elif)

---
+++ tiposDeDatos/condicionales/elif.py
@@ -0,0 +1,19 @@
+ingreso_mensual = 72000
+gasto_mensual = 80000
+
+if ingreso_mensual > 10000:
+    if ingreso_mensual - gasto_mensual < 0:
+        print("estas en deficit")
+    else:
+        print("y pa, estas gastando una banda, hay que ver si te alcanza")
+
+elif ingreso_mensual > 1000:
+    print("estas bien en latinoamerica")
+
+elif ingreso_mensual > 500:
+    print("estas bien en Argentina")
+    
+elif ingreso_mensual > 200:
+    print("estas bien en Venezuela")
+else:
+    print("SOS POBRE")

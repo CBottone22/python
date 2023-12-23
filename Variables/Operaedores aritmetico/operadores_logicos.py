e1f6c6ef1fafbeea749c3a6508d482785d39f5e7
BCaroBottone <125994571+BCaroBottone@users.noreply.github.com>
2023-12-23 07:08:34 +0000

    AND OR NOT

---
+++ Variables/Operaedores aritmetico/operadores_logicos.py
@@ -0,0 +1,18 @@
+#AND
+
+Resultado = True & True #Devolver True
+Resultado = False & True #Devolver Falso
+Resultado = True & False #Devolver Falso
+Resultado = False & False #Devolver Falso
+
+#OR
+
+Resultado = True & True #Devolver True
+Resultado = False & True #Devolver True
+Resultado = True & False #Devolver True
+Resultado = False & False #Devolver Falso
+
+#NOT
+
+Resultado = not True #Devolver Falso
+Resultado = not False #Devolver True

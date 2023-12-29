0cec4b57dc60579a6dd64272dfddc44273700684
BCaroBottone <125994571+BCaroBottone@users.noreply.github.com>
2023-12-29 03:00:58 +0000

    metodos de diccionarios

--- public/Metodos/metodos_de_diccionarios.py
+++ public/Metodos/metodos_de_diccionarios.py
@@ -1,9 +1,23 @@
+#keys() devuelve las claves y tb sirve para iterar
 diccionario = {
     "nombre": 'Caro',
     "apodo": 'gorda',
     "subs": '10000000'
 }
 
+#nos dewvuelve un objeto dict_item
 claves = diccionario.keys()
 
-print(claves)
+#obteniendo un elemento con get() (si no encuentra nada el programa contrinua)
+claves = diccionario.get("nombre")
+
+#eliminando todo el diccionario
+#diccionario.clear()
+
+#eliminando un elemento del diccionario
+#diccionario.pop("nombre","subs")
+
+#obteniendo una dict_items iterable
+diccionario_iterable = diccionario.items()
+
+print(diccionario_iterable)

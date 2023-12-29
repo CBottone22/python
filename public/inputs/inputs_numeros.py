cb8df02d03446e8124ce7d8ebf2f3fb83fadc83e
BCaroBottone <125994571+BCaroBottone@users.noreply.github.com>
2023-12-29 03:41:04 +0000

    input numeros

---
+++ public/inputs/inputs_numeros.py
@@ -0,0 +1,11 @@
+#pedirle un numereo al usuario
+numero = input("dame un numero: ")
+
+#convirtiendo el numero a entero y lo multiplico en 2
+#resultado_entero = int(numero)*2
+
+#convirtiendo el numero a flotante y lo multiplico en 2
+resultado_flotante = float(numero)*2
+
+#mostrando el resultado
+print(resultado_flotante)

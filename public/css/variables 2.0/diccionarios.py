53b68ea5e783c3b2a335c9e86f7dd6ba82d2d1ed
CBottone22 <133555445+CBottone22@users.noreply.github.com>
2024-01-03 01:37:37 +0000

    Variuables 2.0

--- public/css/variables 2.0/diccionarios.py
+++ public/css/variables 2.0/diccionarios.py
@@ -4,7 +4,10 @@ diccionario = dict(nombre="Caro",apodo="gorda")
 #las listas no pueden ser claves y usamos frozenset para meter conjuntos
 diccionario = {frozenset(["caro","gorda"]):"jajaja"}
 
-#creando diccionarios con fronkeys
-diccionario = dict.fronkeys(["nombre","apodo"])
+#creando diccionarios con fromkeys() valor por defecto none
+diccionario = dict.fromkeys(["nombre","apodo"])
+
+#creando diccionarios con fromkeys() cambiando el valor por defecto a "No se"
+diccionario = dict.fromkeys(["nombre","apodo"],"No se")
 
 print(diccionario)

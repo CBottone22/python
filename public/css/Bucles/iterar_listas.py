54c96de02e2c0a254e7d3179db343690b4010a46
CBottone22 <133555445+CBottone22@users.noreply.github.com>
2024-01-03 03:05:12 +0000

    Bucles

---
+++ public/css/Bucles/iterar_listas.py
@@ -0,0 +1,11 @@
+animales = ["gato","perro","pez","loro","cocodrilo"]
+numeros = [52,16,32,14]
+
+#recorriendo la lista animales
+for animal in animales:
+    print(f'ahora la variable animal es igual a: {animal}')
+  
+#recorriendo la lista numeros y multiplicando cada valor por 10    
+for numero in numeros:
+    resultado = numero * 10
+    print(resultado)

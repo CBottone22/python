ac738cd62424b975ef84c757abac47abab28f392
CBottone22 <133555445+CBottone22@users.noreply.github.com>
2024-01-04 08:40:04 +0000

    Bucles

---
+++ public/css/Bucles/blucle_While.py
@@ -0,0 +1,10 @@
+#Creando un contador que va a ir sumandose
+contador = 0
+
+#mientras la condicion se cumpla el while se va a ejecutar
+#vuelta, tras vuelta se verifica la condicion
+while contador < 10:
+    contador += 1
+    print(contador)
+    
+print("el while llego a su fin")
--- public/css/Bucles/mas_iteraciones.py
+++ public/css/Bucles/mas_iteraciones.py
@@ -1,5 +1,7 @@
+#creando las listas
 cadena ="Hola Gorda"
 frutas = ["banana","manzana","ciruela","pera","naranja","granada","durazno"]
+numeros = [2,5,8,10]
 
 #evitando que se coma un granada con la sentencia continue
 for fruta in frutas:
@@ -17,3 +19,7 @@ else:
 #recorrer(recorrer es igual a iterar) una cadena de texto
 for letra in cadena:
     print(letra)
+     
+#for en una sola linea de codigo (duplicamos los numeros)
+numeros_duplicados = [x*2 for x in numeros]
+print(numeros_duplicados)

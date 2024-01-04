048a415ac57194927324fa6c608402ebc69aa2f8
CBottone22 <133555445+CBottone22@users.noreply.github.com>
2024-01-04 10:31:44 +0000

    Funciones

---
+++ public/Funciones/02-crear_funciones.py
@@ -0,0 +1,12 @@
+#Creando una funcion simple
+#def saludar():
+    #print("Hola Gorda ¿como andas?")
+
+#Ejecutando la funcion simple   
+#saludar()
+
+#Crear una funcion que tenga parametros
+def saludar(nombre):
+    print(f"Hola {nombre} ¿Como andas?")
+    
+saludar("Maca")

dbe881ed3e020c24b0a512e647db2ab448703171
CBottone22 <133555445+CBottone22@users.noreply.github.com>
2024-01-05 03:09:28 +0000

    creando funciones con return

--- public/Funciones/02-crear_funciones.py
+++ public/Funciones/02-crear_funciones.py
@@ -6,7 +6,34 @@
 #saludar()
 
 #Crear una funcion que tenga parametros
-def saludar(nombre):
-    print(f"Hola {nombre} ¿Como andas?")
+def saludar(nombre, sexo):
+    sexo = sexo.lower()
+    if (sexo == "mujer"):
+        adjetivo = "reina"
+    elif (sexo == "hombre"):
+        adjetivo = "rey"
+    else:
+        adjetivo = "amor"
+    print(f"Hola {nombre}, mi {adjetivo} ¿Como andas?")
     
-saludar("Maca")
+saludar("Maca","mujer")
+saludar("Alvaro","hombre")
+saludar("Fran","no binario")
+
+#Crear una funcion que nos retorne valores
+def crear_contraseña_random(num):
+    chars = "abcdfghij"
+    num_entero = str(num)
+    num = int(num_entero[0])
+    c1 = num - 2
+    c2 = num
+    c3 = num - 5
+    contraseña = f'{chars[c1]}{chars[c2]}{chars[c3]}{num*2}'
+    num = 2
+    #print(contraseña)
+    #para que nos quede oculta a nosotrosque sea algo de nuestro sistema debe ser
+    return contraseña
+
+password = crear_contraseña_random(2)
+frase = f"Tu contraseña nueva es: {password}"
+print(frase)

a051e77c9801b12b39f696bf9da8d80b332fe5bc
CBottone22 <133555445+CBottone22@users.noreply.github.com>
2024-01-06 07:33:10 +0000

    Ejercicios 2

--- public/Ejercicios_2/Ejercicio2-1.py
+++ public/Ejercicios_2/Ejercicio2-1.py
@@ -1,12 +1,33 @@
 #Falto el profe y los chicos van a armar la clase
 
-#Pedir el nombre y la edad de los compañeros que vinieron hoy a clases
-def obtener_compañeros(cantidad):
+#Función para obtener el asistente y el profesor según la edad
+def obtener_compañeros(cantidad_de_compañeros):
+    
+    #Creando la lista de compañeros
     compañeros = []
+    
+    #ejecutando un for para pedir informacion de cada compañero
     for i in range(cantidad_de_compañeros):
-        nombre = input("ingrese el nombre del compañero")
-        edad = int(input("ingrese la edad del compañero"))
+        nombre = input("ingrese el nombre del compañero: ")
+        edad = int(input("ingrese la edad del compañero: "))
         compañero = (nombre,edad)
-        compañeros = append(compañero)
+        
+        #agregando la informacion a la lista
+        compañeros.append(compañero)
+        
+    #ordenando de menor a mayor su edad
     compañeros.sort(key=lambda x:x[1])
+    
+    #compañeros[x] devuelve una tupla con (nombre,edad) y despues accedemos al nombre
+    #para definir al asistente y al profesor
     asistente = compañeros[0][0]
+    profesor = compañeros[-1]
+    
+    #retornamos la tupla
+    return asistente,profesor
+
+#desempaquetamos lo que nos retorna la función
+asistente,profesor = obtener_compañeros(5)
+
+#mostrando el resultado
+print(f"El profesor es: {profesor} y su asistente es: {asistente}")

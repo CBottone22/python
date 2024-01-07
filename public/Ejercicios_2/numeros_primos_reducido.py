2314a5340a8b879f61f694245abf1f6fb76ee5d2
CBottone22 <133555445+CBottone22@users.noreply.github.com>
2024-01-07 02:10:48 +0000

    Ejercicios2

---
+++ public/Ejercicios_2/numeros_primos_reducido.py
@@ -0,0 +1,5 @@
+primos_hasta = lambda num: list(filter(lambda x: all(x % i != 0 for i in range(2, int(x == 0.5) +1)), range(2,num)))
+                            
+print(primos_hasta(15))
+
+#muestra[2,3,5,7,11,13]                             
